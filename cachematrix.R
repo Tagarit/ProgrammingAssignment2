45cf76006ac44a3cee7b22291d11897ed9681a72
Tagarit Ariza <tangerine.ariza@gmail.com>
2016-04-03 18:49:07 -0500

    cacheMatrix .R

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,25 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
-                 cache <- NULL
+                 # stores the cached value
+                 # initialize to NULL                                 
+                 m <- NULL
+                 
+                 # create a matrix 
                  set <- function (y){
+                                                 
                             x <<- y
-                            cache <<- NULL
+                            m <<- NULL
                  }
+                 
+                 # get the value of the matrix
                  get <- function () x
-                 setmatrix <- function(solve) cache <<- solve
-                 getmatrix <- function()cache
+                 # invert the matrix and store in m
+                 setmatrix <- function(solve) m <<- solve
+                 # get the inverted matrix from m
+                 getmatrix <- function()m
+                 
+                 # return the created functions to the working environment
                  list(set=set, get=get,
                     setmatrix=setmatrix,
                     getmatrix=getmatrix)
@@ -23,15 +34,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x = matrix(), ...) {
-        ## Return a matrix that is the inverse of 'x'
-                 cache <- x$getmatrix()
+        # Return a matrix that is the inverse of 'x'
+                 m <- x$getmatrix()
+                 # return the inverted matrix if it exists
                  if (!is.null(cache)){
                             message("getting cached data")
+                                                 
+                            # dislplays the matrix on the console                     
                             return(cache)
                  }
+                 
+                 # creates a matrix since it does not exist
                  matrix <- x$get()
-                 cache <- solve(matrix, ...)
-                 x$setmatrix(cache)
-                 cache
+                 
+                 # set and return the inverse of a matrix 
+                 m <- solve(matrix, ...)
+                 
+                 # set the inverted matrix in 'm' 
+                 x$setmatrix(m)
+                 
+                 # displays the matrix on the console
+                 m
                  }
 
