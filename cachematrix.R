731418aa415b8dd0a5a11ec0877b6968639bc5e8
Tagarit Ariza <tangerine.ariza@gmail.com>
2016-04-03 15:37:32 -0500

    cachematri.R file Programming Assignment 2 - Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a square inverible matrix object that can
+## cache its inverse
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+                   cache <- NULL
+                   set <- function (y){
+                                x <<- y
+                                cache <<- NULL
+                   }
+                   get <- function () x
+                   setmatrix <- function(solve) cache <<- solve
+                   getmatrix <- function()cache
+                   list(set=set, get=get,
+                        setmatrix=setmatrix,
+                        getmatrix=getmatrix)
+} 
 
-}
 
+## This function computes the inverse of the matrix returned 
+## by makeCacheMatrix.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x = matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+                   cache <- x$getmatrix()
+                   if (!is.null(cache)){
+                                message("getting cached data")
+                                return(cache)
+                   }
+                   matrix <- x$get()
+                   cache <- solve(matrix, ...)
+                   x$setmatrix(cache)
+                   cache
+                   }
+
